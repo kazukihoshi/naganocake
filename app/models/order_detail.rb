a611c61ea7c2226541ea5da41766bee520fe2688
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-07-29 12:21:10 +0000

    [add]ルーティング、enumの確認

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,4 @@
 class OrderDetail < ApplicationRecord
+  
+  enum is_active: {cannot_atart: 0, waiting_for_production: 1, production: 2,production_completed: 3}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
+  namespace :admin do
     get 'orders/show'
   end
+  
   namespace :public do
     get 'orders/new'
     get 'orders/index'
@@ -9,23 +17,28 @@ Rails.application.routes.draw do
     get 'orders/confirm'
     get 'orders/complete'
   end
+  
   namespace :public do
     get 'cart_items/index'
   end
+  
   namespace :admin do
     get 'genres/index'
     get 'genres/edit'
   end
+  
   namespace :admin do
     get 'items/index'
     get 'items/show'
     get 'items/new'
     get 'items/edit'
   end
+  
   namespace :public do
     get 'items/index'
     get 'items/show'
   end
+  
   devise_for :admins,skip: [:registrations,:passwords], controllers:{
     sessions: "admin/sessions"
   }
