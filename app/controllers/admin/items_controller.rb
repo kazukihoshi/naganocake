0824cd73fd202f06e8a4c5104be5074eae685ac4
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-07-31 21:06:02 +0000

    [add]ヘッダーの作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -17,7 +17,7 @@ class Admin::ItemsController < ApplicationController
   private
   
   def item_params
-    params.require(:item).permit(:name,:introduction,:price,:image)
+    params.require(:item).permit(:name,:introduction,:price,:image,:is_active,:genre_id)
   end
   
   
