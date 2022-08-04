fa0050c291b6094386fb995eb168c01ffb36fd7a
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-08-04 12:51:42 +0000

    [add]注文履歴詳細画面の作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def show
-    @orders = Order.all
     @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
   
   def update
