320c27fcc65be1c585c0098151152c89e36a5cc2
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-08-09 12:48:43 +0000

    [add]customerの退会処理

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -10,16 +10,19 @@ class Public::CustomersController < ApplicationController
   
   def update
     customer = current_customer
-    customer.update(current_customer)
-    redirect_to customers_my_page(current_customer)
+    customer.update(customer_params)
+    redirect_to customers_my_page_path(current_customer)
   end  
 
   def unsubscribe
   end
   
+  def withdraw
+  end
+  
   private
   def customer_params
-    params.require(:customer).permit()
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
   
 end
