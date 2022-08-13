d8f86a7b53318d32d047b466d107a30872ab7efa
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-08-13 11:29:53 +0000

    [add]addressのeditの作成

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,24 @@
 class Public::AddressesController < ApplicationController
+  def index
+    @address = Address.new
+    @addresses = Address.all
+    #@address.save
+    #redirect_to edit_address_path(current_customer.id)
+  end
+  
+  def create
+    address = Address.new
+    address.save
+    redirect_to edit_address_path(current_customer.id)
+  end
+  
+  private
+  
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address)
+  end
+  
+  
+    
+  
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :addresses, dependent: :destroy
 
   def full_name
     self.last_name + self.first_name
