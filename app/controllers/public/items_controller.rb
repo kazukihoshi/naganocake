1c423ba3f0b8600d993e8e4500223c537ab58234
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-08-14 13:01:10 +0000

    [add]itemのshow作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,5 +5,14 @@ class Public::ItemsController < ApplicationController
   end
 
   def show
+    @genres = Genre.all
+    @item = Item.find(params[:id])
   end
+  
+  
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price)
+  end
+  
 end
