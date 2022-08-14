04044c2fff8760fc2147f655f9df56a955536108
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-08-14 09:13:12 +0000

    [add]itemのshow、個数選択

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,15 +1,33 @@
 class Public::AddressesController < ApplicationController
   def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses
     #@address.save
     #redirect_to edit_address_path(current_customer.id)
   end
   
   def create
-    address = Address.new
+    address = Address.new(address_params)
+    #p address
+    address.customer_id = current_customer.id
     address.save
-    redirect_to edit_address_path(current_customer.id)
+    redirect_to addresses_path
+  end
+  
+  def edit
+    @address = Address.find(params[:id])
+  end
+  
+  def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
+  end
+  
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
   
   private
