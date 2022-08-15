9eb769609c4d1d4b09fee374c7059822491c91a1
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-08-15 07:49:46 +0000

    [add]cart_itemのindex、select_tagの設定

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -12,6 +12,23 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
   
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
+  end
+  
+  def update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update
+    redirect_to cart_items_path
+  end
+  
+  def destroy_all
+    cart_item.destroy_all
+    redirect_to items_path
+  end
+  
   private
   
   def cart_item_params
