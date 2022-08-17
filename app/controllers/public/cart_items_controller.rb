a80362e32c464490bae5f5c9f2a8894e036c69c1
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-08-17 12:45:27 +0000

    [add]cart_item作成、destroy_allのエラー

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -6,12 +6,29 @@ class Public::CartItemsController < ApplicationController
   end
   
   def create
-    @cart_item = CartItem.new(cart_item_params)
+    #binding.pry
+    #@cart_item = CartItem.new(cart_item_params)
     #p cart_item
-    @cart_item.customer_id = current_customer.id
+    #@cart_item.customer_id = current_customer.id
+    ## @cart_item.save
+    #redirect_to cart_items_path
+    @cart_item = current_customer.cart_items.find_by(item_id: cart_item_params[:item_id])
+    if @cart_item
+      @cart_item.amount += cart_item_params[:amount].to_i
+    else
+      @cart_item = current_customer.cart_items.new(cart_item_params)
+    end
+    
+    
     @cart_item.save
     redirect_to cart_items_path
+    
   end
+  
+  def destroy_all
+    current_customer.cart_item.destroy_all
+    redirect_to items_path
+  end  
   
   def destroy
     cart_item = CartItem.find(params[:id])
@@ -25,14 +42,9 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path
   end
   
-  def destroy_all
-    cart_item.destroy_all
-    redirect_to items_path
-  end
-  
   private
   
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount, :customer_id)
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
