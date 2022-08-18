40a50f64f1c18f7b624d30e6b6d6f24254807133
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-08-18 12:45:29 +0000

    [add]order/newからorder/showへのリンクエラー

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,6 +2,7 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     @order.customer_id = current_customer.id
+    redirect_to orders_confirm_path
   end
 
   def index
@@ -11,11 +12,35 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @order = Order.new(order_params)
+    @order = Order.new
     @address = Address.find(params[:order][:address_id])
     @order.postal_code = @address.postal_code
     @order.address = @address.address
     @order.name = @address.name
+    # @order.select_address = @address.select_address
+    # @order.address_id = @address.address_id
+    
+    if params[:order][:select_address] == 0
+      @order.postal_code = current_customer.postal_code
+      @order.customer.address = current_customer.address
+      @order.name = current_customer.last_name + current_customer.first_name
+      
+    elsif params[:order][:select_address] == 1
+      @address = Address.find(params[:order][:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
+      
+    elsif params[:order][:select_address] == 2
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
+    else
+      render 'new'
+    end
+    
+    @order.save
+    
   end
 
   def complete
@@ -23,7 +48,7 @@ class Public::OrdersController < ApplicationController
   
   private
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :select_address, :address_id)
   end
   
 end
