d67356cd49e9157be0b8a9a5aedcd095713006e5
Kazuki Hoshi <106673481+kazukihoshi@users.noreply.github.com>
2022-08-19 04:57:33 +0000

    [add]ヘッダーのエラー、退会処理

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -21,6 +21,12 @@ class Public::SessionsController < Devise::SessionsController
 
   protected
 
+  def costomer_state
+    @customer = Customer.find_by(email: params[:customer][:email])
+    return if!@customer
+    if @customer.valid_password?(params[:customer][:password])
+  end
+  
   
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_in_params
